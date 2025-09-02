11db681133a6245ddb98a3b37f1ce10050eb1a30
unknown <martino.maranzana495@gmail.com>
2025-09-02 17:27:20 +0200

    WIP: save changes before switching

--- src/glitch/ppu_glitch.h
+++ src/glitch/ppu_glitch.h
@@ -4,14 +4,14 @@
 #include <stdint.h>
 
 /* Glitch control - change to 1 to enable, 0 to disable */
-#define PPU_GLITCH_ENABLED 0
+#define PPU_GLITCH_ENABLED 1
 
 /* Apply simulated hardware faults to a PPU address.
-   Example: A05 stuck low forces bit 5 to 0. */
+   Example: A07 stuck low forces bit 7 to 0. */
 static inline uint16_t ppu_glitch_addr(uint16_t addr)
 {
 #if PPU_GLITCH_ENABLED
-    return addr & ~0x0020; /* force A05 = 0 */
+    return addr & ~0x0080; /* force A07 = 0 */
 #else
     return addr; /* glitch disabled - return address unchanged */
 #endif
